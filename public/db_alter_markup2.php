5c3f72a25d2c954d1dbe675d6edef91ce9f64b2e
Michael Espinoza <michael@example.com>
2026-04-14 23:54:09 -0500

    Desc table

--- public/db_alter_markup2.php
+++ public/db_alter_markup2.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../src/models/Database.php';
 
 try {
     $db = Database::getInstance();
-    $results = $db->fetchAll("SHOW INDEX FROM article_markup");
+    $results = $db->fetchAll("DESCRIBE article_markup");
     print_r($results);
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
